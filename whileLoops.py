e8263db62d06c06dc036494d619a959b80f060c8
Gonzalez <agonzalez206@cps.k12.il.us>
2024-05-07 09:13:41 -0500

    loops2

---
+++ whileLoops.py
@@ -0,0 +1,24 @@
+#for num in nums:
+ #   for letter in 'abc':
+   #     for letter2 in 'abc':
+   #         for letter3 in 'abc':
+   #                 for letter4 in 'abc':
+   #                     print(num, letter, letter2, letter3, letter4)
+#for i in range (1, 11):
+#    print(i)
+# x=0
+
+# while x < 10:
+#   if x== 5:
+#    break
+#    print(x)
+#     x += 1
+while True:
+    print("Enter your name")
+    name = input( )
+    if name == 'your name' :
+        print("your name is" +name)
+        break
+    else:
+        print("Thank You!")
+
